2127e281e35364630877a9af5d0b072cc154947f
chenhaifeng <m@cheft.cn>
2016-08-05 15:12:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <input v-model="msg">
+    <h1>Hello {{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Cheft!'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+  color: #35495E;
+}
+
+input {
+  display: block;
+  width: 200px;
+  height: 30px;
+  font-size: 20px;
+  border: 1px solid #41B883;
+  color: #35495E;
+}
+</style>
